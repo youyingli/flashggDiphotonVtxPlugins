15c358d66ff7536240640699e6412f37cdb0fe0f
Youying <luffy7804@gmail.com>
2018-04-06 22:23:35 +0200

    Rename directories

--- Validation/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/flashggDiphotonVtxPlugins/validation/',1)[0])+'/cfipython/slc6_amd64_gcc630/flashggDiphotonVtxPlugins/validation')
